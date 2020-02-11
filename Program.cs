c1e8f130c2e0b43eb1e7682cc4bcc67d50a623d2
hardosilalahi <hardosilalahi@gigaming.io>
2020-02-11 18:20:04 +0700

    finish all literally

--- Program.cs
+++ Program.cs
@@ -24,72 +24,17 @@ namespace async
             // Console.WriteLine(await numfour.movieRobertHolland());
             // Console.WriteLine(await numfour.movie2016());
 
-            var scrape = new numsix();
-            await scrape.StartAsync();
+            // var scrape = new numsix();
+            // await scrape.StartAsync();
 
             // var scrape2 = new numfive();
             // await scrape2.StartAsync();
 
-            //start of num2
-        //     var numTwo = await numtwo.Fetch();
-        //     Console.WriteLine("gaji karyawan lebih dari Rp15.000.000 :");
-        //     List<string> sal = new List<string>();
-        //     foreach (var X in numTwo){
-        //         if(X.salary>15000000){
-        //             sal.Add($"{X.first_name} {X.last_name}");
-        //         }
-        //     }
-        //     Console.WriteLine(String.Join(", ", sal));
-        //     Console.WriteLine();
-            
-        //     Console.WriteLine("yang tinggal di jkt :");
-        //     List<string> jkt48 = new List<string>();
-        //     foreach (var X in numTwo){
-        //         foreach(var Y in X.addresses){
-        //             if(Y.city== "DKI Jakarta"){
-        //                 jkt48.Add($"{X.first_name} {X.last_name}");
-        //             }
-        //         }
-        //     }
-        //     var jkt96 = jkt48.Distinct();
-        //     Console.WriteLine(String.Join(", ", jkt96));
-        //     Console.WriteLine();
-
-        //     Console.WriteLine("yang ultah maret :");
-        //     List<string> Mar = new List<string>();
-        //     foreach (var X in numTwo){
-        //         if ((X.birthday).Substring(5,2)=="03"){
-        //             Mar.Add($"{X.first_name} {X.last_name}");
-        //         }
-        //     }
-        //     Console.WriteLine(String.Join(", ", Mar));
-        //     Console.WriteLine();
-
-        //     Console.WriteLine("yang di departemen R&D :");
-        //     List<string> RnD = new List<string>();
-        //     foreach (var X in numTwo){
-        //         if(X.department.name== "Research and development"){
-        //             RnD.Add($"{X.first_name} {X.last_name}");
-        //         }
-        //     }
-        //     Console.WriteLine(String.Join(", ", RnD));
-        //     Console.WriteLine();
-
-        //     Console.WriteLine("yang absen di oktober :");
-        //     List<int> Absen = new List<int>();
-        //     foreach (var X in numTwo){
-        //         int count = 0;
-        //         foreach(var Y in X.presence_list){
-        //             if(Y.Substring(5,2)=="10"){
-        //                 count++;
-        //             } 
-        //         }
-        //         Absen.Add(count);
-        //     }
-        //     Console.WriteLine(String.Join(", ", Absen));
-        // }
-        //end of num2
-
+            Console.WriteLine(await numtwo.GajiLebih());
+            Console.WriteLine(await numtwo.TinggalJakarta());
+            Console.WriteLine(await numtwo.BirthMarch());
+            Console.WriteLine(await numtwo.Dept_RD());
+            Console.WriteLine(await numtwo.OctAbsent());
 
         }
     }   
--- numtwo.cs
+++ numtwo.cs
@@ -12,31 +12,26 @@ using System.IO;
 
 namespace async
 {
-      public class Address
-    {
+      public class Address{
         public string label { get; set; }
         public string address { get; set; }
         public string city { get; set; }
     }
 
-    public class Phone
-    {
+    public class Phone{
         public string label { get; set; }
         public string phone { get; set; }
     }
 
-    public class Department
-    {
+    public class Department{
         public string name { get; set; }
     }
 
-    public class Position
-    {
+    public class Position{
         public string name { get; set; }
     }
 
-    public class RootObject2
-    {
+    public class Employed{
         public int id { get; set; }
         public string avatar_url { get; set; }
         public string employee_id { get; set; }
@@ -44,7 +39,7 @@ namespace async
         public string last_name { get; set; }
         public string email { get; set; }
         public string username { get; set; }
-        public string birthday { get; set; }
+        public DateTime birthday { get; set; }
         public List<Address> addresses { get; set; }
         public List<Phone> phones { get; set; }
         public List<string> presence_list { get; set; }
@@ -55,15 +50,83 @@ namespace async
 
     public class numtwo
     {
-        public static async Task<List<RootObject2>> Fetch(){
+        public static async Task<List<Employed>> StartingClient(){
             var client = new HttpClient();
 
             var result = await client.GetStringAsync("https://mul14.github.io/data/employees.json");
 
-            var fet = JsonConvert.DeserializeObject<List<RootObject2>>(result);
+            var deserial = JsonConvert.DeserializeObject<List<Employed>>(result);
 
-            return fet;
+            return deserial;
+        }
+
+        public static async Task<string> GajiLebih(){
+            var start = await StartingClient();
+            var employeeName = new List<string>();
+
+            foreach(var i in start){
+                if(i.salary > 15000000){
+                    employeeName.Add($"{i.first_name} {i.last_name}");
+                }
+            }
+            return $"Gaji Karyawan Lebih dari Rp15.000.000 : {String.Join(',', employeeName)}";
+        }
+
+        public static async Task<string> TinggalJakarta(){
+            var start = await StartingClient();
+            var employeeName = new List<string>();
+
+            foreach(var i in start){
+                foreach(var j in i.addresses){
+                    if(j.city == "DKI Jakarta"){
+                        employeeName.Add($"{i.first_name} {i.last_name}");
+                    }
+                }
+            }
+            var result = employeeName.Distinct();
+            return $"Karyawan yang tinggal di JKT: {String.Join(',', result)}";
         }
         
+        public static async Task<string> BirthMarch(){
+            var start = await StartingClient();
+            var employeeName = new List<string>();
+
+            foreach(var i in start){
+                var month = i.birthday.Month;
+                if(month == 03){
+                    employeeName.Add($"{i.first_name} {i.last_name}");
+                }
+            }
+            var result = employeeName.Distinct();
+            return $"Karyawan yang Ulangtahun Bulan Maret : {String.Join(',', result)}";
+        }
+
+        public static async Task<string> Dept_RD(){
+            var start = await StartingClient();
+            var employeeName = new List<string>();
+
+            foreach(var i in start){
+                if(i.department.name == "Research and development"){
+                    employeeName.Add($"{i.first_name} {i.last_name}");
+                }
+            }
+            return $"Karyawan Dept. R&D : {String.Join(',', employeeName)}";
+        }
+
+        public static async Task<string> OctAbsent(){
+            var start = await StartingClient();
+            var Absencies = new List<int>();
+
+            foreach(var i in start){
+                var count = 0;
+                foreach(var j in i.presence_list){
+                    if(j.Contains("-10-") == true){
+                        count++;
+                    }
+                }
+                Absencies.Add(count);
+            }
+            return $"Absen di Bulan Oktober : {String.Join(',', Absencies)}";
+        }
     }
 }
